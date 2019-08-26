5a1db4e5e70cee247653938d8a4aadf2b4732a31
NamTran <nam.tran.set15@eiu.edu.vn>
2019-08-26 16:03:52 +0700

    update routes

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,11 +1,8 @@
 const studentController = require("./controllers/StudentController");
 const scheduleController = require("./controllers/ScheduleController");
-<<<<<<< HEAD
 const AttendanceController = require("./controllers/AttendanceController");
 const modulesController = require("./controllers/ModulesController");
-=======
 const allTasksController = require("./controllers/AllTasksController");
->>>>>>> getAllTasks
 const cors = require('cors');
 
 module.exports = (app) => {
@@ -16,13 +13,10 @@ module.exports = (app) => {
     app.route('/schedule')
         .get(scheduleController.get);
 
-<<<<<<< HEAD
     app.route('/modules')
         .get(modulesController.getModules);
 
     app.route('/attendance')
         .get(AttendanceController.getAttendance);
-=======
     app.route('/allTasks').get(allTasksController.getAll);
->>>>>>> getAllTasks
 };
