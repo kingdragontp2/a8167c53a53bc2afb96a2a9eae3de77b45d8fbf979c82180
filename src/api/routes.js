fec005f3478faaa1e2daf6a3aa1347e276458319
nghianguyen1997 <nghia.nguyen.241297@gmail.com>
2019-08-26 15:46:13 +0700

    Revert "Nghia"
    This reverts commit b7632d374dd80d2bd3c601856c48c3a092f30bb7.

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,12 +1,17 @@
 const studentController = require("./controllers/StudentController");
 const scheduleController = require("./controllers/ScheduleController");
+const allTasksController = require("./controllers/AllTasksController");
+const studentModulesList = require("./controllers/StudentModulesList");
 const cors = require('cors');
 
 module.exports = (app) => {
     app.use(cors());
     app.route('/student')
-        .get(studentController.getstudentinfo);
+        .get(studentController.getStudentInfo);
 
     app.route('/schedule')
         .get(scheduleController.get);
+
+    app.route('/allTasks').get(allTasksController.getAll);
+    app.route('/modulesList').get(studentModulesList.get);
 };
